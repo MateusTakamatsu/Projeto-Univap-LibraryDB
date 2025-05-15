88ebc6cc7de3e1dda71d72472812625a39fd1f07
MateusTakamatsu <mateus.takamatsu.dev@gmail.com>
2025-05-15 20:26:57 -0300

    Improving rebuild_db.sh with improvements to show result

--- rebuild_db.sh
+++ rebuild_db.sh
@@ -6,14 +6,20 @@ case $action in
 	d)
 		echo "Dropping database..."
 		mariadb -e "DROP DATABASE library_db;"
+		echo "Showing result..."
+		mariadb -e "SHOW DATABASES;"
 		;;
 	c)
 		echo "Creating database..."
 		mariadb < ./create.sql
+		echo "Showing result..."
+		mariadb library_db -e "SHOW TABLES;"
 		;;
 	p)
 		echo "Populating database..."
 		mariadb library_db < ./populate.sql
+		echo "Showing result..."
+		mariadb library_db -e "SELECT * FROM volumes_rentals;"
 		;;
 	*)
 		echo "???????"
