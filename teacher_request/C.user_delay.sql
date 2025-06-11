de1b2da3ef987546529f0c8a105d6759a9cf8546
GustavoBehnck <gustavobehnckcardoso@gmail.com>
2025-06-11 18:39:43 -0300

    Updating to use order by

--- teacher_request/C.user_delay.sql
+++ teacher_request/C.user_delay.sql
@@ -2,4 +2,5 @@ SELECT c.name as nome, DATEDIFF(CURDATE(),r.planned_date) as atraso_em_dias,r.pl
 FROM rentals r
 JOIN clients c on c.id = r.client_id
 LEFT JOIN `returns` re on re.rental_id = r.id
-WHERE re.id IS NULL;
+WHERE re.id IS NULL
+ORDER BY atraso_em_dias DESC;
