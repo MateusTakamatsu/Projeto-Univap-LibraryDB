e52fb07221712bd61fcee8decc3fe47cdb8c7c32
GustavoBehnck <gustavobehnckcardoso@gmail.com>
2025-06-11 19:06:59 -0300

    updating file name and B logic

--- queries/B.10_best_rentals.sql
+++ queries/B.10_best_rentals.sql
@@ -2,6 +2,6 @@ SELECT COUNT(vr.volume_id) as Emprestimos, p.title as titulo
 FROM volumes_rentals vr
 JOIN volumes v ON v.id = vr.volume_id
 JOIN pieces p on p.id = v.piece_id
-GROUP BY v.id
+GROUP BY p.id
 ORDER BY Emprestimos DESC
 LIMIT 10;
